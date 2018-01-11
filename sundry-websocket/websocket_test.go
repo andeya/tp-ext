85ce91e77e963468c9fdf9f31b187deed54d5585
henrylee2cn <henrylee_cn@foxmail.com>
2018-01-11 10:39:45 +0800

    Add websocket

---
+++ sundry-websocket/websocket_test.go
@@ -0,0 +1,45 @@
+package websocket_test
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	tp "github.com/henrylee2cn/teleport"
+	websocket "github.com/henrylee2cn/tp-ext/sundry-websocket"
+)
+
+type Args struct {
+	A int
+	B int `param:"<range:1:>"`
+}
+
+type P struct{ tp.PullCtx }
+
+func (p *P) Divide(args *Args) (int, *tp.Rerror) {
+	return args.A / args.B, nil
+}
+
+func TestWebsocket(t *testing.T) {
+	srv := tp.NewPeer(tp.PeerConfig{})
+	http.Handle("/ws", websocket.NewServeHandler(srv, nil))
+	go http.ListenAndServe("0.0.0.0:9090", nil)
+	srv.PullRouter.Reg(new(P))
+	time.Sleep(time.Second * 1)
+
+	cli := tp.NewPeer(tp.PeerConfig{}, websocket.NewDialPlugin("/ws"))
+	sess, err := cli.Dial("127.0.0.1:9090")
+	if err != nil {
+		t.Fatal(err)
+	}
+	var reply int
+	rerr := sess.Pull("/p/divide", &Args{
+		A: 10,
+		B: 2,
+	}, &reply).Rerror()
+	if rerr != nil {
+		t.Fatal(rerr)
+	}
+	t.Logf("10/2=%d", reply)
+	time.Sleep(time.Second)
+}
