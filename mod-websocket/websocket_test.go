eabebd49580ff202ea66ad684957c2c017d5e543
chenmin <453733011@qq.com>
2018-03-09 19:11:46 +0800

    optmixze

--- mod-websocket/websocket_test.go
+++ mod-websocket/websocket_test.go
@@ -9,7 +9,6 @@ import (
 	websocket "github.com/henrylee2cn/tp-ext/mod-websocket"
 	jsonSubProto "github.com/henrylee2cn/tp-ext/mod-websocket/jsonSubProto"
 	pbSubProto "github.com/henrylee2cn/tp-ext/mod-websocket/pbSubProto"
-	jsonproto "github.com/henrylee2cn/tp-ext/proto-jsonproto"
 )
 
 type Args struct {
@@ -23,33 +22,9 @@ func (p *P) Divide(args *Args) (int, *tp.Rerror) {
 	return args.A / args.B, nil
 }
 
-func TestJsonWebsocket(t *testing.T) {
-	srv := tp.NewPeer(tp.PeerConfig{})
-	http.Handle("/ws", websocket.NewServeHandler(srv, nil, jsonproto.NewProtoFunc))
-	go http.ListenAndServe("0.0.0.0:9090", nil)
-	srv.RoutePull(new(P))
-	time.Sleep(time.Second * 1)
-
-	cli := tp.NewPeer(tp.PeerConfig{}, websocket.NewDialPlugin("/ws"))
-	sess, err := cli.Dial("127.0.0.1:9090", jsonproto.NewProtoFunc)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var reply int
-	rerr := sess.Pull("/p/divide", &Args{
-		A: 10,
-		B: 2,
-	}, &reply).Rerror()
-	if rerr != nil {
-		t.Fatal(rerr)
-	}
-	t.Logf("10/2=%d", reply)
-	time.Sleep(time.Second)
-}
-
 func TestJsonSubWebsocket(t *testing.T) {
 	srv := tp.NewPeer(tp.PeerConfig{})
-	http.Handle("/ws", websocket.NewServeHandler(srv, nil, jsonSubProto.NewJsonSubProtoFunc))
+	http.Handle("/ws", websocket.NewJsonServeHandler(srv, nil))
 	go http.ListenAndServe("0.0.0.0:9090", nil)
 	srv.RoutePull(new(P))
 	time.Sleep(time.Second * 1)
@@ -73,7 +48,7 @@ func TestJsonSubWebsocket(t *testing.T) {
 
 func TestPbSubWebsocket(t *testing.T) {
 	srv := tp.NewPeer(tp.PeerConfig{})
-	http.Handle("/ws", websocket.NewServeHandler(srv, nil, pbSubProto.NewPbSubProtoFunc))
+	http.Handle("/ws", websocket.NewPbServeHandler(srv, nil))
 	go http.ListenAndServe("0.0.0.0:9090", nil)
 	srv.RoutePull(new(P))
 	time.Sleep(time.Second * 1)
